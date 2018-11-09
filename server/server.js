b152c57eea8d6c253e33f04ba0522bed218c7dcb
Eugenio Vorontsov <maceuge@icloud.com>
2018-11-09 17:22:25 -0300

    Socket IO Basico

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 const path = require('path');
-
-const app = express();
-
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 
+io.on('connection', (client) => {
+    console.log(`Usuario conectado!`);
 
-app.listen(port, (err) => {
+    client.on('disconnect', () => {
+        console.log(`Usuario se desconecto!`);
+    });
 
+    client.on('sendMsg', (msg) => {
+        console.log(`Mensaje del ${msg.name} que dice: ${msg.msg}`);
+
+        io.emit('recibido', {
+            message: 'Recepcion del sever correcta'
+        });
+    });
+});
+
+
+
+server.listen(port, (err) => {
     if (err) throw new Error(err);
-
     console.log(`Servidor corriendo en puerto ${ port }`);
-
 });
